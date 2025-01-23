55071ed6820aae94869b25a1cb63e3f83894edb1
Diego Lizana <helgait@usal.es>
2025-01-23 12:25:01 +0100

    Started program to clean downloads folder

---
+++ DownloadFolderCleaner/DownloadCleaner.py
@@ -0,0 +1,24 @@
+# Import module os to access different functions that allow to work with out operative system
+import os
+# Import module shutil to do complex operations in the repositories like move, copy files etc
+import shutil
+
+# Define main and add the routes for our different folders
+# Will decide where to move each file depending on the extension
+def main():
+    # 
+    ruta_descargas = "C:/Users/Diego/Downloads"  # Route to download folder (The one we want to clean and organize)
+
+    carpetas_destino = {
+        ('.jpg', '.jpeg', '.png', '.gif', '.bmp'): "F:/DESCARGAS/imagenes",
+        ('.pdf', '.doc', '.docx', '.txt', '.xls', '.xlsx', '.ppt', '.pptx'): "F:/DESCARGAS/documentos",
+        ('.mp4', '.avi', '.mkv', '.mov'): "F:/DESCARGAS/videos",
+        ('.mp3', '.wav', '.aac', '.flac'): "F:/DESCARGAS/musica",
+        ('.zip', '.rar', '.7z', '.tar', '.gz'): "F:/DESCARGAS/comprimidos",
+    } # We specify which files we will move to each folder
+
+    carpeta_otros = "F:/DESCARGAS/otros"  # All the files with non-specified extensions will go to this folder
+
+# Call main to run the program
+if __name__ == "__main__":
+    main()
